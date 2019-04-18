30e587242f6401d63ad3aa3aa957a077868f9bae
Listshe <shenhaogit@aliyun.com>
2019-04-18 21:53:58 +0800

    up

--- testaaa/src/testaaa/aaa.java
+++ testaaa/src/testaaa/aaa.java
@@ -4,6 +4,5 @@ public class aaa {
 	public static void main(String[] args) {
 		System.out.println("bbbbbbbbbbbbbbbbb");
 		System.out.println("bbbbbbbbbbbbbbbbb");
-		System.out.println("bbbbbbbbbbbbbbbbb");
-	}
+			}
 }
