ebf5ae6cc8959ec4db89f737d8bd57cfe13bfa84
Listshe <shenhaogit@aliyun.com>
2019-04-18 20:31:02 +0800

    update

--- testaaa/src/testbbb/aaa.java
+++ testaaa/src/testbbb/aaa.java
@@ -2,7 +2,7 @@ package testbbb;
 
 public class aaa {
 	public static void main(String[] args) {
-		System.out.println("a");System.out.println("a");System.out.println("a");System.out.println("a");
+		System.out.println("a");
 		
 	}
 }
