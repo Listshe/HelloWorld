86cb461b3c0d778ca4ac06f9dabc1b888c6319b3
Listshe <13601777467@163.com>
2019-04-18 19:14:29 +0800

    test3

--- testaaa/src/testaaa/aaa.java
+++ testaaa/src/testaaa/aaa.java
@@ -3,7 +3,6 @@ package testaaa;
 public class aaa {
 	public static void main(String[] args) {
 		System.out.println("a");
-		System.out.println("a");System.out.println("a");System.out.println("a");
-		System.out.println("a");System.out.println("a");System.out.println("a");
+		
 	}
 }
